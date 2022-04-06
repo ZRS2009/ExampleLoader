5a8fb931ba842cce257749940192e5f42f30be6e
SunHo <65275070+ZRS2009@users.noreply.github.com>
2022-04-06 17:00:07 +0200

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOT
+{
+    class Login
+    {
+        public static void Menu()
+        {
+            // Logo ASCII
+            Console.ForegroundColor = ConsoleColor.Red;
+            string logo = @"
+▓█████ ▒██   ██▒ ▄▄▄       ███▄ ▄███▓ ██▓███   ██▓    ▓█████ 
+▓█   ▀ ▒▒ █ █ ▒░▒████▄    ▓██▒▀█▀ ██▒▓██░  ██▒▓██▒    ▓█   ▀ 
+▒███   ░░  █   ░▒██  ▀█▄  ▓██    ▓██░▓██░ ██▓▒▒██░    ▒███   
+▒▓█  ▄  ░ █ █ ▒ ░██▄▄▄▄██ ▒██    ▒██ ▒██▄█▓▒ ▒▒██░    ▒▓█  ▄ 
+░▒████▒▒██▒ ▒██▒ ▓█   ▓██▒▒██▒   ░██▒▒██▒ ░  ░░██████▒░▒████▒
+░░ ▒░ ░▒▒ ░ ░▓ ░ ▒▒   ▓▒█░░ ▒░   ░  ░▒▓▒░ ░  ░░ ▒░▓  ░░░ ▒░ ░
+ ░ ░  ░░░   ░▒ ░  ▒   ▒▒ ░░  ░      ░░▒ ░     ░ ░ ▒  ░ ░ ░  ░
+   ░    ░    ░    ░   ▒   ░      ░   ░░         ░ ░      ░   
+   ░  ░ ░    ░        ░  ░       ░                ░  ░   ░  ░
+                                                             ";
+
+            Console.WriteLine(logo);
+
+            
+
+        }
+    }
+}
---
+++ MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOT
+{
+     class MainMenu
+    {
+        static void Main(string[] args)
+        {
+            // logo and text
+            Login.Menu();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            string text1 = "Example loader made by insolepasteur#9999";
+            string option1 = "\n[1] Discord\n";
+
+            Console.WriteLine(text1);
+
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine(option1);
+            string choose = Console.ReadLine();
+
+            // option 1 open discord
+            if (choose == "1")
+            {
+                var prc = new ProcessStartInfo("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+                 prc.Arguments = ("https://discord.gg/uR9UDq7JVd");
+                Process.Start(prc);
+            }
+
+            Console.ReadLine();
+
+            // Radom console title character generator
+            if (text1 == text1)
+            {
+                while (true)
+                {
+                    Console.Title = "5656858ADZSZZZDD8495";
+                    Console.Title = "03L.YUKUUK58470495";
+                    Console.Title = "2831724143425495";
+                    Console.Title = "34RHRHTHTGH7575795";
+                    Console.Title = "0JZFZFZF738495";
+                    Console.Title = "802453JRNNEUIJFZF95";
+                    break;
+                }
+            }
+        }
+    }
+}
